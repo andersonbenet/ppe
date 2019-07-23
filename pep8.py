4d723896168cd1b19285842aab90ac3db02bd3e2
Anderson Benet <anderson_benet@sicredi.com.br>
2019-07-23 11:31:02 -0300

    Initial commit

---
+++ pep8.py
@@ -0,0 +1,26 @@
+'''
+PEP8 - Python Enhancement Proposal
+'''
+
+class Calculadora:
+    pass
+
+
+class CalculadoraCientifica:
+    pass
+
+
+def soma():
+    pass
+
+
+def soma_dois():
+    pass
+
+
+numero = 4
+
+numero_aux = 5
+
+
+
