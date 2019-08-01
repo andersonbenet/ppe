d1fab373d25bcf115f4a560a94db9d12c3e547a6
Anderson Benet <anderson_benet@sicredi.com.br>
2019-08-01 15:31:47 -0300

    new task - funcion whith return

--- pep8.py
+++ pep8.py
@@ -1,6 +1,8 @@
-'''
-PEP8 - Python Enhancement Proposal
-'''
+"""
+Conhecendo pep8
+python 3
+"""
+
 
 class Calculadora:
     pass
@@ -14,13 +16,53 @@ def soma():
     pass
 
 
-def soma_dois():
+def soma_impar():
     pass
 
 
 numero = 4
+idade = 35
 
-numero_aux = 5
+numero_impar = 5
+name = 'Anderson'
+
+
+if 'a' in 'banana':
+    print ('tem')
+
+print(name)
+print(f'{name}')
+
+#cast
+# idade = int(input("Qual sua idade?"))
+# print(f'Você nasceu em {2019 - idade}')
 
 
 
+nome = 'Testing'
+lista = [1,2,3,4,5]
+numeros = range(1,10)
+
+for letra in nome:
+    print(letra)
+
+for numero in lista:
+    print(numero)
+
+for numero in range(1,10):
+    print(numero)
+
+
+def falar_ola():
+    return "oi"
+
+
+
+print(falar_ola())
+
+for i in range(5):
+    falar_ola()
+
+
+def quadrado_de_7():
+    print(7*7)
